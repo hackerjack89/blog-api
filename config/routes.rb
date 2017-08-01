cee511c59f2a81edd1f4cb0efec61c014497ece7
Prathamesh Doiphode <prathamesh@itsalve.in>
2017-08-01 18:53:47 +0530

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
       resources :comments, :users, :posts,  defaults: {format: :json}
     end
   end
+  root to: "posts#index", defaults: {format: :json}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
